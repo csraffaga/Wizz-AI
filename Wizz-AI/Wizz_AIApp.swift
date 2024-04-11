ba007905d616a50b22d2ab090184ddf22a80ab4b
Gabe <gabriel.aj.raffa@gmail.com>
2024-04-10 21:58:59 -0500

    Initial Commit

---
+++ Wizz-AI/Wizz_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wizz_AIApp.swift
+//  Wizz-AI
+//
+//  Created by Gabriel on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Wizz_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
